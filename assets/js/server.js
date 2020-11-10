a19a95e732a93d94dccb87ca0370b038d99b9a6b
bak1roff01 <bakirovgulomjon@gmail.com>
2020-11-10 14:50:40 +0300

    Create main section page

--- assets/js/server.js
+++ assets/js/server.js
@@ -1,6 +1,7 @@
 const menuToggle = document.querySelector(".menu-toggle");
 const menu = document.querySelector(".site-header__top-menu-list")
 const menuActiveLinks = document.querySelectorAll('.site-header__top-link');
+const menuActiveLink = document.querySelector('.site-header__top-link');
 const toUp = document.querySelector('.toUp');
 
 
@@ -10,10 +11,12 @@ menuToggle.addEventListener('click', function(){
   menu.classList.toggle('sitenav--close')
 })
 
-
 function changeActiveLink(){
   menuActiveLinks.forEach(link => link.classList.remove('site-menu__active'));
   this.classList.add('site-menu__active');
+  menuToggle.classList.remove('active');
+  menu.classList.toggle('sitenav--open')
+  menu.classList.toggle('sitenav--close')
 }
 
 menuActiveLinks.forEach(link => link.addEventListener('click', changeActiveLink));
