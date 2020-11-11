2adfc70a312041d6bd1060a124fba21be70d281c
bak1roff01 <bakirovgulomjon@gmail.com>
2020-11-11 09:04:35 +0300

    Create pricing section

--- assets/js/server.js
+++ assets/js/server.js
@@ -3,6 +3,13 @@ const menu = document.querySelector(".site-header__top-menu-list")
 const menuActiveLinks = document.querySelectorAll('.site-header__top-link');
 const menuActiveLink = document.querySelector('.site-header__top-link');
 const toUp = document.querySelector('.toUp');
+const btn = document.querySelectorAll('.pricing--btn');
+const btn1 = document.querySelector('.disable1');
+const btn2 = document.querySelector('.disable2');
+const btnClose = document.querySelector('.btn--close')
+const monthlyCard = document.querySelector('.Monthly-card');
+const annualyCard = document.querySelector('.Annualy-card');
+const disable = btnClose.getAttribute('disable');
 
 
 menuToggle.addEventListener('click', function(){
@@ -18,6 +25,28 @@ function changeActiveLink(){
   menu.classList.toggle('sitenav--open')
   menu.classList.toggle('sitenav--close')
 }
+
+
+
+function changeActiveBtn(){
+  btn.forEach(b => b.classList.remove('btn--close'));
+  this.classList.add('btn--close');
+  if(btn1.hasAttribute('disabled')){
+    btn1.removeAttribute('disabled');
+    btn2.setAttribute('disabled', 'disabled')
+  }else{
+    btn2.removeAttribute('disabled')
+    btn1.setAttribute('disabled', 'disabled')
+  }
+  monthlyCard.classList.toggle('select');
+  annualyCard.classList.toggle('select');
+}
+
+
+
+
+
+btn.forEach(b => b.addEventListener('click', changeActiveBtn));
 
 menuActiveLinks.forEach(link => link.addEventListener('click', changeActiveLink));
 
